1d560cd83e23db720c96f5ae66e7a0e17326cc8f
Dipendra Pratap <dipendrapratap155@gmail.com>
2021-11-20 05:00:22 -0800

    'adhka'

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running"
+    return "Flask app is running,to kaise hain app"
 
 port = int(os.getenv("PORT", 5000))
 
